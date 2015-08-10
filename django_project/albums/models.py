933bcb0b801af7d86ae793a30527da18d8b2db49
Christian Urcuqui <ulcamilo@gmail.com>
2015-08-10 12:14:40 -0500

    Nuevos modelos

--- django_project/albums/models.py
+++ django_project/albums/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from artists.models import Artist
 # Create your models here.
+
+
+class Albums(models.Model):
+    title = models.CharField(max_length=255)
+    cover = models.ImageField(upload_to='albums')
+    artist = models.ForeignKey(Artist)
--- django_project/artists/models.py
+++ django_project/artists/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True)
+    biography = models.CharField(blank=True)

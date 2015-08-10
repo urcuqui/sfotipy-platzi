85d8e0c61a9eba3a84ddd6041444563f96ecc85a
Christian Urcuqui <ulcamilo@gmail.com>
2015-08-10 16:30:40 -0500

    Informacion

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
+    #url(r'^$', 'django_project.tracks.views', name='hola'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- django_project/tracks/views.py
+++ django_project/tracks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+#def hola(request):
+#    mejor= 'Diseno Web'
+#    return render_to_response (template.html, {'nombre':'Daniel Zavala', 'edad':22, 'cursos': [mejor,'frontend', 'backend', 'estrategia']})
+
